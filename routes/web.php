4b2d8a8f630f6261e1f6fed16bb0a6283b3ead15
Muhammad Tsaqif Mukhayyar <m3g3nz2@gmail.com>
2022-04-22 18:10:09 +0700

    solve auth::routes problem first

--- routes/web.php
+++ routes/web.php
@@ -88,5 +88,8 @@ Route::group(['prefix'=>'admin'],function(){
     Route::group(['prefix'=>'dsyirkah_nonaktif'], function(){
 
     });
-    Auth::routes();
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 });
