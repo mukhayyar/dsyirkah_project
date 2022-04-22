34659d879cfa9a1cd0c20029698967de74480318
Muhammad Tsaqif Mukhayyar <m3g3nz2@gmail.com>
2022-04-22 18:24:32 +0700

    fix routes again sorry typo

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::group(['prefix'=>'admin'],function(){
 
     });
     // Authentication Routes...
-    Route::get('login', [App\Http\Controllers\Admin\Auth\LoginController::class,'showLoginForm'])->name('login');
-    Route::post('login', [App\Http\Controllers\Admin\Auth\LoginController::class,'login']);
-    Route::post('logout', [App\Http\Controllers\Admin\Auth\LoginController::class,'logout'])->name('logout');
+    Route::get('login', [App\Http\Controllers\Auth\LoginController::class,'showLoginForm'])->name('login');
+    Route::post('login', [App\Http\Controllers\Auth\LoginController::class,'login']);
+    Route::post('logout', [App\Http\Controllers\Auth\LoginController::class,'logout'])->name('logout');
 });
