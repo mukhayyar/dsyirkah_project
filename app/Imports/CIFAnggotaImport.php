9b1793444dc6f29a698f661e80e9d27377f71732
Muhammad Tsaqif Mukhayyar <m3g3nz2@gmail.com>
2022-06-18 19:42:23 +0700

    update cif import

--- app/Imports/CIFAnggotaImport.php
+++ app/Imports/CIFAnggotaImport.php
@@ -37,7 +37,23 @@ class CIFAnggotaImport implements ToModel, WithValidation, SkipsOnFailure, Skips
             $user->save();
             Anggota::where('nomor_ba','=',$row[1])->update([
                 'user_id' => $user->id,
-                'no_ktp' => $row[5]
+                'no_ktp' => $row[5],
+                'no_npwp' => $row[6],
+                'jenis_kelamin' => $row[7],
+                'tempat_lahir' => $row[8],
+                'tanggal_lahir' => $row[9],
+                'status_nikah' => $row[10],
+                'alamat_ktp' => $row[11],
+                'kelurahan_ktp' => $row[12],
+                'kecamatan_ktp' => $row[13],
+                'kota_ktp' => $row[14],
+                'provinsi_ktp' => $row[15],
+                'alamat_tinggal' => $row[16],
+                'alamat_domisili' => $row[17],
+                'kelurahan_domisili' => $row[18],
+                'kecamatan_domisili' => $row[19],
+                'kota_domisili' => $row[20],
+                'provinsi_domisili' => $row[21],
             ]);
         }
     }
